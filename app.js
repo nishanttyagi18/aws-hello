969ce695dfdfc5f15ffe97e5bf462f8141621e11
Nishant Tyagi <tyagi11n@gmail.com>
2021-02-14 19:44:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('This app is running on amazon EC2')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
